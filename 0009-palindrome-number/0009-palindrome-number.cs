d1fd7380063e5a5c0d248bae31e652bebacaec24
Sreelekshmi S Amma <42946926+SreelekshmiNair@users.noreply.github.com>
2025-01-22 10:50:59 -0600

    Time: 31 ms (5.02%), Space: 35.2 MB (35.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cs
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public bool IsPalindrome(int x) {
         int res = x,rem=0;
-        if(x<0) return false;
-        while(res!=0){
+        //if(x<0) return false;
+        while(res>0){
            rem=(rem*10) + (res%10);
-           res=res/10;
+           res/=10;
        }
        Console.WriteLine(rem);
-       if(rem==x)return true;
-       else return false;
+       return rem==x;
     }
 }
