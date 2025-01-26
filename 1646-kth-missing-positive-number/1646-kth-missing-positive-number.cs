cf5305abff99ac6672c6c5cc71b4ff2aedc02743
Sreelekshmi S Amma <42946926+SreelekshmiNair@users.noreply.github.com>
2025-01-25 18:48:25 -0600

    Time: 0 ms (100%), Space: 42.5 MB (26.39%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cs
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int FindKthPositive(int[] arr, int k) {
+
+        //Using Binary search, time complexity is O(logN)
         int left = 0, right = arr.Length-1;
         while(left<=right){
             
@@ -9,7 +11,11 @@ public class Solution {
             //number at that position since the actual number which was supposed to take that
             //position was position + 1 (since 0 based index).
 
-             /*If the number of missing integers before mid is less than k, then we update the left pointer to mid + 1; otherwise, we update the right pointer to mid - 1. We continue this process until the left pointer crosses the right pointer, at which point the left pointer will be the index of the first missing integer. Finally, we add k to the left pointer to get the k-th missing integer.*/
+             /*If the number of missing integers before mid is less than k, then we update the
+             // left pointer to mid + 1; otherwise, we update the right pointer to mid - 1.
+             // We continue this process until the left pointer crosses the right pointer, at
+             // which point the left pointer will be the index of the first missing integer. 
+             //Finally, we add k to the left pointer to get the k-th missing integer.*/
 
             int missingNum = arr[mid]-mid-1;
            // Console.WriteLine("Missing num is "+missingNum);
